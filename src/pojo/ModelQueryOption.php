f6c4c2955bafca7ac168c1678e73e66184492009
mapenghui <3072609949@qq.com>
2021-08-07 21:37:11 +0800

    完善SmartThink查询构造系统#4

--- src/pojo/ModelQueryOption.php
+++ src/pojo/ModelQueryOption.php
@@ -10,14 +10,14 @@ class ModelQueryOption
     private $options = [];
 
     public function set($key, $value) {
-        $this->options->$key = $value;
+        $this->options[$key] = $value;
     }
 
     public function get($key, $default = null) {
-        if(!isset($this->options->$key)) {
+        if(!isset($this->options[$key])) {
             return $default;
         }
 
-        return $this->options->$key;
+        return $this->options[$key];
     }
 }
